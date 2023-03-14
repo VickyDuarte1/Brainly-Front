b05b5b4a3d995d75e624bd187b3f89305ad8b36e
CaroRuvira <caroruvira@gmail.com>
2023-03-14 16:02:47 -0300

    Creación Aboout y NavBar incompletos

---
+++ client/src/Components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+    <div>
+        <h1>Sobre Brainly</h1>
+        <h2>Brainly es lo mas, porque somos lo mas</h2>
+        <h3>Ojalá no tengas un tumor cerebral 💕</h3>
+    </div>
+    );
+}
+
+export default About;
