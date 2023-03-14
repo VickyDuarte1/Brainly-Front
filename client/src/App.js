6b111641fec6d31afb114036127d0d3590e5946c
CaroRuvira <caroruvira@gmail.com>
2023-03-14 12:47:42 -0300

    Creación de Landing Page, componente y ruta

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './Components/LandingPage';
+import Home from './Components/Home';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path= "/" component = {LandingPage} />
+        <Route  exact path="/home" component={Home} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Components/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+
+const LandingPage = () => {
+    return (
+        <div >
+            <div >
+              
+                <span>Bienvenido a Brainly</span>
+
+               <div>
+                    <Link to = '/home'>
+                        <button>INGRESAR</button>
+                    </Link>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default LandingPage;
