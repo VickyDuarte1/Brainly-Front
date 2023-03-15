e4773f9061fece1fbec3c838190c695d0a42ff41
CaroRuvira <caroruvira@gmail.com>
2023-03-15 12:55:11 -0300

    Actualización Home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,15 +1,34 @@
-// import React from "react";
+import React from "react";
 import NavBar from "../NavBar/NavBar";
+import Card from "../Card/Card";
 
 
 export default function Home() {
     return (
-
+      <div>
         <div>
-          <div>
-            <NavBar/>
-          </div>
-          <h1>Bienvenido al home </h1>
+          <NavBar/>
         </div>
+
+        <h1>BRAINLY</h1>
+        <Card
+        title="Diagnóstico preciso de tumores"
+        text="Sube tus imágenes de tomografías y obtén un diagnóstico preciso de tumores en cuestión de minutos gracias a nuestro algoritmo de inteligencia artificial altamente preciso."
+        imageSrc="https://via.placeholder.com/150"
+      />
+      <Card
+        title="Resultados en tiempo real"
+        text="Obtén los resultados de diagnóstico de tus pacientes en tiempo real para que puedas tomar decisiones informadas sobre su tratamiento."
+        imageSrc="https://via.placeholder.com/150"
+      />
+      <Card
+        title="Seguridad y privacidad de los datos"
+        text="Garantizamos la seguridad y privacidad de los datos de tus pacientes mediante el uso de las últimas tecnologías en encriptación y protección de datos."
+        imageSrc="https://via.placeholder.com/150"
+      />
+     </div>
       );
 }
+
+
+
