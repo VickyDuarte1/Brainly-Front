50f2fbf344ccf1f2b15c190e5d2ca9502835bfe3
VickyDuarte1 <victoria.durte@gmail.com>
2023-03-25 16:01:48 -0300

    coments

--- client/src/Components/Comentarios/Coment.jsx
+++ client/src/Components/Comentarios/Coment.jsx
@@ -4,10 +4,10 @@ import '../../Redux/coments.json'
 export default function Coment( {id, nombre, correo, comentario, tipo_usuario } ) {
  
   return (
-    <div className='doctor'>
+    <div className='comments'>
      <p>ID: {id}</p>
-      <h2 className='d-name'>Nombre: {nombre}</h2>
-      <h3 className='d-email'> {correo}</h3>
+      <h2 className='nombre-u'>Nombre: {nombre}</h2>
+      <h3 className='correo-u'> {correo}</h3>
       <h4> comentario: {comentario}</h4>
       <div>{tipo_usuario}</div>
     </div>
