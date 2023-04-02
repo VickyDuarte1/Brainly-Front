ebe47ad6d521c393077fe819c4adb9371ea1665d
VickyDuarte1 <112184349+VickyDuarte1@users.noreply.github.com>
2023-04-02 18:33:24 -0300

    Update actions.js

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,6 +1,5 @@
-import { GET_USERS, GET_DOCTORS,ADD_USER , GET_COMMENTS } from "./action-types";
+import { GET_USERS, GET_DOCTORS,ADD_USER , GET_COMMENTS, CREATE_POST } from "./action-types";
 import axios from 'axios';
-import commentsData from './coments.json';
 
 export const getDoctors = () =>{
   return function(dispatch){
@@ -27,6 +26,19 @@ export function getUsers() {
   )
 }};
 
+export const createPost = (postData) => {
+  return (dispatch) => {
+    axios.post('https://brainly-back.onrender.com/comentarios', postData)
+      .then(response => {
+        dispatch({ type: CREATE_POST , payload: response.data });
+      })
+      .catch((error) => {
+       console.log(error);;
+      });
+  };
+};
+
+
 export const createUser = (payload) => {
   return function(dispatch) {    
     // Agregar usuario al array correspondiente en el estado del store
@@ -40,12 +52,16 @@ export const createUser = (payload) => {
     }
   }
 
-  export const getComments = () => {
-    return dispatch => {
-      const comments = commentsData.comentarios;
-      dispatch({
-        type: GET_COMMENTS,
-        payload: comments
-      });
-    };
-  };
+  export const getComments = () =>{
+    return function(dispatch){
+      axios.get(`https://brainly-back.onrender.com/get_comments`)
+      .then((response) => {
+          console.log(response.data);
+          
+          // Imprime el resultado de la respuesta en la consola
+          return dispatch({type: GET_COMMENTS, payload: response.data})
+        }).catch((error) => {
+          console.log(error)
+        })
+      }
+    }
